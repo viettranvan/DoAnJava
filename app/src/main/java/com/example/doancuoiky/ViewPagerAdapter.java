81443eebf14de36cee9ea100a7500864274b1efd
viettranvan2k <viettranvan2k@gmail.com>
2020-10-12 16:14:55 +0700

    init bottom tab with viewpager

---
+++ app/src/main/java/com/example/doancuoiky/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.doancuoiky;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new CartFragment();
+            case 2:
+                return new SearchFragment();
+            case 3:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
